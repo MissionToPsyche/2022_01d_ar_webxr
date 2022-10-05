c4e12ae36ee39582220fcab8038e90f51940146f
Tayler <tbaird2@asu.edu>
2022-10-05 13:49:53 -0500

    Abstact out code that was in .html file

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const THREE = require('./../js/three.js');
+const assert = require('assert');
+
+describe('The THREE object', function() {
+    it('should have a defined BasicShadowMap constant', function() {
+      assert.notEqual('undefined', THREE.BasicShadowMap);
+    }),
+  
+    it('should be able to construct a Vector3 with default of x=0', function() {
+      const vec3 = new THREE.Vector3();
+      assert.equal(0, vec3.x);
+    })
+  })
+
+  /*
+describe('SourceAR.js object', function() {
+    it('should contain an object', function() {
+        assert.notEqual('undefined', sourceAR.scene);
+    } )
+} )
+*/
