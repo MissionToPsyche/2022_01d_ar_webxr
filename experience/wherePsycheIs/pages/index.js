462b09840aba500b815502ad9bec5b6360de5242
Lizell Cabalquinto <lizellc@yahoo.com>
2022-11-09 14:27:25 -0800

    add entry screen

---
+++ experience/wherePsycheIs/pages/index.js
@@ -0,0 +1,7 @@
+// document.querySelector('#startMission').addEventListener('click', function (evt) {
+//     alert('This 2D element was clicked!');
+//   });
+
+function marsExplore() {
+    alert("Mars Explore");
+}
