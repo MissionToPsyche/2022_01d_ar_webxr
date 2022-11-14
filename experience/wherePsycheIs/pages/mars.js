a9c8c6edce36ec39afad4e3703a90483254a9699
Lizell Cabalquinto <lizellc@yahoo.com>
2022-11-13 17:33:49 -0800

    add mars flyby page

---
+++ experience/wherePsycheIs/pages/mars.js
@@ -0,0 +1,9 @@
+console.log("mars.js successfully connected");
+
+var marsFlyByContainer = document.querySelector("#marsFlyByContainer");
+
+function click_explore() {
+    document.getElementById("marsContainer").style.display = "none";
+    document.getElementById("marsPsycheButton").style.display = "block";
+}
+
