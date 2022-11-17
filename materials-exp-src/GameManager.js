353de522bc9ee4c5d12f56f7ecb82b30fdfb29be
Rachel Corey <rcoreyde@asu.edu>
2022-11-16 20:32:26 -0700

    basic screen navigation working

--- materials-exp-src/GameManager.js
+++ materials-exp-src/GameManager.js
@@ -17,8 +17,8 @@ export class GameManager {
     aFrameDivConst;
 
     constructor() {
-        this.overlayDivConst = document.getElementById("overlay");
-        this.aFrameDivConst = document.getElementById("aFrameSpan");
+        this.overlayDivConst = document.getElementById("overlaySpan");
+        this.aFrameDivConst = document.getElementById("aFrameScene");
         this.buildScreens();        
     }
 
@@ -31,27 +31,95 @@ export class GameManager {
     buildScreens() {
         // build screens here
         var overlayDiv = `Hello from Inserted Overlay Div!`;
-        var aFrameDiv = `    <a-scene background="color: #ECECEC"
-             webxr="optionalFeatures: dom-overlay; overlayElement: #overlay">
-            <a-box position="0 1 -3" rotation="0 45 15" color="#ff00ea" shadow></a-box>
-            </a-scene>`;
+        // var aFrameDiv = `<a-node><a-box position="0 1 -3" rotation="0 45 15" color="#ff00ea" shadow></a-box></a-node>`;
+        var aFrameDiv = document.createElement("a-entity");
+        aFrameDiv.innerHTML = `<a-box position="0 1 -3" rotation="0 45 15" color="#ff00ea" shadow></a-box>`;
         var instructions = ["Testing instructions array"];
+        var overlayDiv2 = `Hello from Inserted Overlay Div 2!`;
+        var aFrameDiv2 = document.createElement("a-entity");
+        aFrameDiv2.innerHTML = ` <a-box id="nextScreen" position="0 1 -3" rotation="0 45 15" color="#ffff00" shadow></a-box>`
         var screen1 = new Screen(overlayDiv, aFrameDiv, instructions);
-        var screensList = [screen1];
+        var screen2 = new Screen(overlayDiv2, aFrameDiv2, instructions);
+        var screensList = [screen1, screen2];
         this.screensCollection = new ScreensCollection(screensList);
-        var newScreen = new Screen();
-        newScreen.testScreenClass();
-        this.screensCollection.testScreensCollectionClass();
         this.buildNextScreen();
+    }
+
+    nextScreen() {
+        this.takedownPrevScreen();
+        this.screensCollection.nextScreen();
+        this.buildNextScreen();
+        this.incrementStage();
     }
 
     buildNextScreen() {
         // build next screen here from the collection
         this.overlayDivConst.innerHTML = this.screensCollection.getCurScreen().getOverlayDiv();
-        this.aFrameDivConst.innerHTML = this.screensCollection.getCurScreen().getAframeDiv();
+        this.aFrameDivConst.appendChild(this.screensCollection.getCurScreen().getAframeDiv());
     }
 
     takedownPrevScreen() {
         // takedown previous screen here
+        this.overlayDivConst.innerHTML = ``;
+        this.aFrameDivConst.removeChild(this.aFrameDivConst.childNodes[0]);
     }
+
+    incrementStage() {
+        this.currentStage++;
+    }
+
+    decrementStage() {
+        this.currentStage--;
+    }
+
+    incrementNickelCounter() {
+        this.nickelCounter++;
+    }
+
+    incrementIronCounter() {
+        this.ironCounter++;
+    }
+
+    incrementOxygenCounter() {
+        this.oxygenCounter++;
+    }
+
+    getNickelCounter() {
+        return this.nickelCounter;
+    }
+
+    getIronCounter() {
+        return this.ironCounter;
+    }
+
+    getOxygenCounter() {
+        return this.oxygenCounter;
+    }
+
+    getNickelNeeded() {
+        return this.nickelNeeded;
+    }
+
+    getIronNeeded() {
+        return this.ironNeeded;
+    }
+
+    getOxygenNeeded() {
+        return this.oxygenNeeded;
+    }
+
+    getIsGameActive() {
+        this.checkIfGameActive();
+        return this.isGameActive;
+    }
+
+    getCurrentStage() {
+        return this.currentStage;
+    }
+
+    getMaxStages() {
+        return this.maxStages;
+    }
+
+
 }
--- materials-exp-src/Main.js
+++ materials-exp-src/Main.js
@@ -1,3 +1,9 @@
 import { GameManager } from './GameManager.js';
 
 const gameManager = new GameManager();
+
+function getNextScreen() {
+    gameManager.nextScreen();
+}
+
+window.getNextScreen = getNextScreen;
